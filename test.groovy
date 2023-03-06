11d5106fd1442d9903325f828e16bb119b20c056
AdrianaDiAvi <adriana.diaz.avila@intel.com>
2023-03-06 12:55:27 -0500

    Added https

--- test.groovy
+++ test.groovy
@@ -10,8 +10,8 @@ pipeline {
         environment {
         GITHUB_CREDS = credentials('one-source-token')
         ONESOURCE_CFWAP_DIR = "pre-silicon-cfwap"
-        ONESOURCE_CFWAP_REPO =   "https://github.com/intel-innersource/applications.infrastructure.services-framework.wl-analysis-initial-checkpoint.git"
-        ONESOURCE_REPO = "https://github.com/intel-innersource/applications.benchmarking.benchmark.platform-hero-features.git"
+        ONESOURCE_CFWAP_REPO =   "github.com/intel-innersource/applications.infrastructure.services-framework.wl-analysis-initial-checkpoint.git"
+        ONESOURCE_REPO = "github.com/intel-innersource/applications.benchmarking.benchmark.platform-hero-features.git"
         ONESOURCE_DIR = "platform-hero-features"
         MAIN_BRANCH='master'
     }
@@ -20,8 +20,8 @@ pipeline {
         stage('Setup Repos'){
             steps {
             sh '''
-            git clone ${GITHUB_CREDS_USR}:${GITHUB_CREDS_PSW}@${ONESOURCE_CFWAP_REPO} ${ONESOURCE_CFWAP_DIR}
-            git clone ${GITHUB_CREDS_USR}:${GITHUB_CREDS_PSW}@${ONESOURCE_REPO} ${ONESOURCE_DIR}
+            git clone https://${GITHUB_CREDS_USR}:${GITHUB_CREDS_PSW}@${ONESOURCE_CFWAP_REPO} ${ONESOURCE_CFWAP_DIR}
+            git clone https://${GITHUB_CREDS_USR}:${GITHUB_CREDS_PSW}@${ONESOURCE_REPO} ${ONESOURCE_DIR}
             rsync -av --exclude .git ${ONESOURCE_DIR} ${ONESOURCE_CFWAP_DIR}/
            '''
             }
