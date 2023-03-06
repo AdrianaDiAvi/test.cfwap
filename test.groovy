455cc74c76aab815c6b69aa330d1f251d0ba3f5f
AdrianaDiAvi <adriana.diaz.avila@intel.com>
2023-03-06 12:48:40 -0500

    Added rsync

--- test.groovy
+++ test.groovy
@@ -12,7 +12,7 @@ pipeline {
         ONESOURCE_CFWAP_DIR = "pre-silicon-cfwap"
         ONESOURCE_CFWAP_REPO =   "https://github.com/intel-innersource/applications.infrastructure.services-framework.wl-analysis-initial-checkpoint.git"
         ONESOURCE_REPO = "https://github.com/intel-innersource/applications.benchmarking.benchmark.platform-hero-features.git"
-        ONESOURCE_DIR = "presilicon"
+        ONESOURCE_DIR = "platform-hero-features"
         MAIN_BRANCH='master'
     }
 
@@ -22,9 +22,9 @@ pipeline {
             sh '''
             mkdir ${ONESOURCE_CFWAP_DIR}
             git clone https://${GITHUB_CREDS_USR}:${GITHUB_CREDS_PSW}@${ONESOURCE_CFWAP_REPO} ${ONESOURCE_CFWAP_DIR}
-            cd ..
             mkdir ${ONESOURCE_DIR}
             git clone https://${GITHUB_CREDS_USR}:${GITHUB_CREDS_PSW}@${ONESOURCE_REPO} ${ONESOURCE_DIR}
+            rsync -av --exclude .git ${ONESOURCE_DIR} ${ONESOURCE_CFWAP_DIR}/
            '''
             }
         }
