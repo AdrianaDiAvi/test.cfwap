58fd2bf7503bdb74e278ffe016c474ed3b811a74
AdrianaDiAvi <adriana.diaz.avila@intel.com>
2023-03-06 17:03:43 -0500

    Test connection SSH

--- test.groovy
+++ test.groovy
@@ -35,11 +35,11 @@ pipeline {
         stage('Setup Repos'){
             steps {
                 script{
-                    sh '''
+                    /*sh '''
                     git clone https://${GITHUB_CREDS_USR}:${GITHUB_CREDS_PSW}@${ONESOURCE_CFWAP_REPO} ${ONESOURCE_CFWAP_DIR}
                     git clone https://${GITHUB_CREDS_USR}:${GITHUB_CREDS_PSW}@${ONESOURCE_REPO} ${ONESOURCE_DIR}
                     rsync -av --exclude .git ${ONESOURCE_DIR} ${ONESOURCE_CFWAP_DIR}/
-                    '''
+                    ''' */
                     sshCommand remote: ion_remote_connection(), command: "cd /nfs/site/disks/wl_analysis_automation_01/aarcemor/jenkins/"
                 }
             }
